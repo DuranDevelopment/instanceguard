f1cb7fd54924a1efe2d0d5d4a9662fea429a69f4
xkaasplakje <57845320+duranaaron@users.noreply.github.com>
2023-08-20 16:05:10 +0200

    Rollback changes made to Listener.java

--- src/main/java/cc/ddev/instanceguard/listener/handler/Listener.java
+++ src/main/java/cc/ddev/instanceguard/listener/handler/Listener.java
@@ -1,40 +1,35 @@
 package cc.ddev.instanceguard.listener.handler;
 
 import cc.ddev.instanceguard.logger.Log;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.Event;
-import net.minestom.server.event.EventNode;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public interface Listener {
     default void register() {
-        EventNode<Event> defaultEventNode = EventNode.all("default-events"); // Create a default EventNode
-        register(defaultEventNode); // Call the method with the default EventNode
-    }
-
-    default void register(EventNode<? extends Event> eventNode) { // Modify the method signature
+        var eventHandler = MinecraftServer.getGlobalEventHandler();
         var methods = this.getClass().getDeclaredMethods();
 
         for (Method method : methods) {
             if (method.isAnnotationPresent(Listen.class)) {
                 var paramTypes = method.getParameterTypes();
                 // Check if the method has a single parameter of type Event or a subclass of Event
-                if (paramTypes.length == 1 && Event.class.isAssignableFrom(paramTypes[0])) {
+                if(paramTypes.length == 1 && Event.class.isAssignableFrom(paramTypes[0])) {
                     // Get the class of the event from the parameter type
                     Class<?> eventToStickTo = paramTypes[0];
                     // Check if the event is a subclass of Event
-                    if (Event.class.isAssignableFrom(eventToStickTo)) {
-                        // Register the event using the provided EventNode
-                        eventNode.addListener(eventToStickTo.asSubclass(Event.class), event -> {
+                    if(Event.class.isAssignableFrom(eventToStickTo)) {
+                        // Register the event
+
+                        eventHandler.addListener(eventToStickTo.asSubclass(Event.class), event -> {
                             try {
                                 method.invoke(this, event);
                             } catch (IllegalAccessException | InvocationTargetException e) {
                                 throw new RuntimeException(e);
                             }
                         });
-                        if (Log.getLogger().isDebugEnabled())
-                            Log.getLogger().debug("Registered listener for event " + eventToStickTo.getSimpleName());
+                        if (Log.getLogger().isDebugEnabled()) Log.getLogger().debug("Registered listener for event " + eventToStickTo.getSimpleName());
                     } else {
                         throw new IllegalArgumentException("Method annotated with @Listen must have a single parameter of type Event or a subclass of Event.");
                     }
